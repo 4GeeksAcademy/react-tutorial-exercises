39a2a2115542a1f68bba75b7f3d0eb4f311b5483
Alejandro Sanchez <aalejo@gmail.com>
2018-11-01 17:22:04 -0400

    Update index.js

--- exercises/01.6-maping-array-to-li/index.js
+++ exercises/01.6-maping-array-to-li/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const animals = ["Horse", "Turtle", "Elephan", "Monkey"];
+const animals = ["Horse", "Turtle", "Elephant", "Monkey"];
 
 /**
  * change the content of the map function
