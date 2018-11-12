7d248c2f2dd5c75d45c46597ade8a1e34bf92333
Alejandro Sanchez <aalejo@gmail.com>
2018-11-12 10:55:34 -0500

    Update index.js

--- exercises/01.4-building-a-layout/index.js
+++ exercises/01.4-building-a-layout/index.js
@@ -2,7 +2,7 @@ import React from "react"; //Main React.js library
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 
 const data = {
-	image: "/assets/bobdylan.png",
+	image: "https://ucarecdn.com/0be9ec85-1ff9-44ab-927e-7ce2c0803753/",
 	cardTitle: "Bob Dylan",
 	cardDescription:
 		"Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.",
