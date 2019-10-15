e8f6a7275e52ead509e80595218f34d2856d21d2
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-10-15 20:17:24 +0000

    completed 04

--- exercises/04-conditional-rendering/tests.js
+++ exercises/04-conditional-rendering/tests.js
@@ -9,11 +9,30 @@ test("ReactDOM needs to be called once", () => {
   expect(ReactDOM.render.mock.calls.length).toBe(1);
 });
 
-test("Tests are not fully done for this exercise", () => {
-  expect("Not Done").toBe("Done");
+
+
+test("The component Alert should return return the exact HTML", () => {
+  const tree = renderer.create(ReactDOM.render.mock.calls[0][0]).toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+<div>
+  <div
+    className="alert alert-danger"
+    role="alert"
+  >
+    OMG! Something really bad has happended!
+  </div>
+  <div
+    className="alert alert-warning"
+    role="alert"
+  >
+    Well, it is not that bad after all!
+  </div>
+</div>
+`);
 });
 
 // test("The component should return return the exact HTML", () => {
 //   const tree = renderer.create(ReactDOM.render.mock.calls[0][0]).toJSON();
 //   expect(tree).toMatchInlineSnapshot();
 // });
+
