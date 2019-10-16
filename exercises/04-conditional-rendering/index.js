16b7a4761f2256388197bd0e9f982546cb09f035
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-10-16 14:22:40 +0000

    updated 04

--- exercises/04-conditional-rendering/index.js
+++ exercises/04-conditional-rendering/index.js
@@ -4,9 +4,11 @@ import PropTypes from "prop-types";
 
 const Alert = props => {
 	//add the condition inside this function
-    return <div class="alert alert-danger" role="alert">
-    This is a primary alert-check it out!
-    </div>
+	return (
+		<div class="alert alert-danger" role="alert">
+			This is a primary alert-check it out!
+		</div>
+	);
 };
 Alert.propTypes = {
 	color: PropTypes.string,
