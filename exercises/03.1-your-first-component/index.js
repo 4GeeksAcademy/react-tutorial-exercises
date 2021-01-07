33e1be34af3670e0aefe43fb42cceba3809fa4a2
Rolando Scarfullery <74724935+hexbreak@users.noreply.github.com>
2021-01-06 19:53:28 -0500

    Update index.js
    minor typo fix

--- exercises/03.1-your-first-component/index.js
+++ exercises/03.1-your-first-component/index.js
@@ -5,5 +5,5 @@ export const PrintHello = () => {
 	return <h1>I Love React</h1>;
 };
 
-// change the syntaxt of the first parameter to make it <PrintHello /> instead of PrintHello()
+// change the syntax of the first parameter to make it <PrintHello /> instead of PrintHello()
 ReactDOM.render(PrintHello(), document.querySelector("#myDiv"));
