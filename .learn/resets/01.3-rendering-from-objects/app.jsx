8ba1014f053b6355d278987ee2ba7892d33861d9
Keaton13 <bajabuggy313@gmail.com>
2021-10-12 11:07:21 -0700

    Finished 1.3

--- .learn/resets/01.3-rendering-from-objects/app.jsx
+++ .learn/resets/01.3-rendering-from-objects/app.jsx
@@ -7,10 +7,7 @@ const customer = {
 };
 
 //              your code inside these <div> tags
-const output = <div>
-	<h1>My name is {customer.first_name}</h1>
-	<h2>My last name is {customer.last_name}</h2>
-</div>;
+const output = <div></div>;
 
 //              what     where
 ReactDOM.render(output, document.querySelector("#myDiv"));
