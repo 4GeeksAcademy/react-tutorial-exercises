d3cdf2e92444c9bafe959ad978c213b323981b71
Keaton13 <bajabuggy313@gmail.com>
2021-10-14 11:43:01 -0700

    Finished 3.5

--- .learn/resets/03.5-alert-component/app.jsx
+++ .learn/resets/03.5-alert-component/app.jsx
@@ -7,13 +7,6 @@ import PropTypes from "prop-types";
  * the html that bootstrap dictates for its alert component
  */
 
-const Alert = (props) => {
-    return (
-        <div className="alert alert-danger" role="alert">
-            {props.text}
-        </div>
-    )
-}
 
 // here is where the alert component is being used, you don't have to edit this part,
 // but it helps you understan what properties is the component using
