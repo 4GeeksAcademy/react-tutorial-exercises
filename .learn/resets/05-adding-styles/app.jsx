aa775d446d4779444423d41de459e68fe7de3413
Keaton13 <bajabuggy313@gmail.com>
2021-10-19 15:02:24 -0700

    Finished 5.0

--- .learn/resets/05-adding-styles/app.jsx
+++ .learn/resets/05-adding-styles/app.jsx
@@ -4,9 +4,7 @@ import PropTypes from "prop-types";
 
 //add the styles here
 const mySuperStyles = {
-	fontSize: "16px",
-	background: "black",
-	border: "1px solid yellow"
+
 };
 
 const Badge = props => {
