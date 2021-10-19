a94306d403ff66eea11f8b7f6e51d6d784e4c711
Keaton13 <bajabuggy313@gmail.com>
2021-10-19 14:57:12 -0700

    Finished 4.2

--- .learn/resets/04.2-conditional-rendering-ii/app.jsx
+++ .learn/resets/04.2-conditional-rendering-ii/app.jsx
@@ -6,8 +6,7 @@ import PropTypes from "prop-types";
 const Alert = props => {
 	const colorClasses = {
 		red: "alert-danger",
-		orange: "alert-warning",
-		green: "alert-success"
+		orange: "alert-warning"
 	};
 	if (colorClasses[props.color] === undefined) alert(`The color ${props.color} is not in the possible list of colors`);
 
