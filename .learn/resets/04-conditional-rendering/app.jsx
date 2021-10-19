0abb4619f0847f8dce831b0234c662960b2e745c
Keaton13 <bajabuggy313@gmail.com>
2021-10-19 14:38:08 -0700

    Finished 4.0

--- .learn/resets/04-conditional-rendering/app.jsx
+++ .learn/resets/04-conditional-rendering/app.jsx
@@ -4,15 +4,12 @@ import PropTypes from "prop-types";
 
 const Alert = props => {
 	//add the condition inside this function
-	if (props.show === false) {
-		return null;
-	} else {
-		return (
-			<div className="alert alert-danger" role="alert">
-				This is a primary alert-check it out!
-			</div>
-		);
-	}
+
+	return (
+		<div className="alert alert-danger" role="alert">
+			This is a primary alert-check it out!
+		</div>
+	);
 };
 Alert.propTypes = {
 	color: PropTypes.string,
