d66cbe4e926936cef8b3125c6c4f706eef7eef65
dsilva06 <diegosilva06092001@gmail.com>
2022-06-02 20:27:58 +0000

    test works fine, made README file more readable

--- exercises/01.3-building-a-layout/app.jsx
+++ exercises/01.3-building-a-layout/app.jsx
@@ -12,8 +12,18 @@ const data = {
 	},
 };
 
-let content = <img src={data.image} />;
-
+let content = (
+	<div className="card m-5">
+		<img alt="Card image cap" src={data.image} className="card-img-top" />
+		<div className="card-body">
+			<h5 className="card-title">{data.cardTitle}</h5>
+			<p className="card-text">{data.cardDescription}</p>
+			<a href={data.button.url} className="btn btn-primary">
+				{data.button.label}
+			</a>
+		</div>
+	</div>
+);
 /**
  * define the variable 'content' here and fill it with the
  * needed code to render the bootstrap card
