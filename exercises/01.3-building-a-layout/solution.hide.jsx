214a5dc38fa63c785296a2ef2931ea6577271e70
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2022-06-06 21:31:17 +0000

    Solution.hide file improved on ex 01.3

--- exercises/01.3-building-a-layout/solution.hide.jsx
+++ exercises/01.3-building-a-layout/solution.hide.jsx
@@ -1,4 +1,19 @@
-<div className="card m-5">
+import React from "react"; //Main React.js library
+import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
+
+const data = {
+	image: "../../.learn/assets/Dylan.png?raw=true",
+	cardTitle: "Bob Dylan",
+	cardDescription:
+		"Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.",
+	button: {
+		url: "https://en.wikipedia.org/wiki/Bob_Dylan",
+		label: "Go to wikipedia",
+	},
+};
+
+let content = (
+	<div className="card m-5">
 		<img alt="Card image cap" src={data.image} className="card-img-top" />
 		<div className="card-body">
 			<h5 className="card-title">{data.cardTitle}</h5>
@@ -8,3 +23,10 @@
 			</a>
 		</div>
 	</div>
+);
+/**
+ * define the variable 'content' here and fill it with the
+ * needed code to render the bootstrap card
+ **/
+
+ReactDOM.render(content, document.querySelector("#myDiv"));
