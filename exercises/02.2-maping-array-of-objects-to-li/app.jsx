1579f0660d64ffde55e050ecd6e293363ea4db89
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-10-11 23:04:24 +0200

    Update app.jsx

--- exercises/02.2-maping-array-of-objects-to-li/app.jsx
+++ exercises/02.2-maping-array-of-objects-to-li/app.jsx
@@ -1,5 +1,5 @@
-import React from "react"; //Main React.js library
-import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
+import React from "react";
+import ReactDOM from "react-dom";
 
 const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
 
@@ -10,7 +10,7 @@ const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
 
 
 
-// 2) add the array planetsInHTML inside the innerHTML of this ul
+// 2) Add the array planetsInHTML inside the innerHTML of this <ul>
 const content = (<ul className="list-group m-5"></ul>);
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
