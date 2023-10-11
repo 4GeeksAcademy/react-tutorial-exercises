9ba67d0cfe8e26508f313ac9f3892dcfad10dd17
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-10-11 21:05:51 +0200

    Create solution.hide.jsx

---
+++ exercises/01-hello-world/solution.hide.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// If we have a variable
+let age = "12";
+let name = "John";
+
+// We can use it in our html like this
+let output = (
+	<span>
+		James is <strong>12</strong> years old
+	</span>
+);
+
+// Use react-dom to render it
+ReactDOM.render(output, document.querySelector("#myDiv"));
