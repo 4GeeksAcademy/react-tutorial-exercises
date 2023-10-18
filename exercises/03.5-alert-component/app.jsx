81273f139300d5d0c95d36d5c4151f86b75bfded
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-10-18 16:45:05 +0200

    Update app.jsx

--- exercises/03.5-alert-component/app.jsx
+++ exercises/03.5-alert-component/app.jsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 /**
- * here you have declare your Alert component and return
+ * Here you have to declare your Alert component and return
  * the html that bootstrap dictates for its alert component
  */
 
 
-// here is where the alert component is being used, you don't have to edit this part,
-// but it helps you understan what properties is the component using
-ReactDOM.render(<Alert text="OMG! Something really bad has happended!" />, document.querySelector("#myDiv"));
+// Here is where the <Alert /> component is being used, you don't have to edit this part
+// But it helps you understand what properties the component is using
+ReactDOM.render(<Alert text="OMG! Something really bad has happened!" />, document.querySelector("#myDiv"));
