2d2a797a908ad4b83ab7da112b3ff23c637cf367
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-10-18 19:06:33 +0200

    Create solution.hide.jsx

---
+++ exercises/04.1-conditional-rendering/solution.hide.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+const Alert = (props) => {
+	// Your component here
+	const colorClasses = {
+		red: "alert-danger",
+		orange: "alert-warning",
+	};
+
+	return (
+		<div className={`alert ${colorClasses[props.color]}`} role="alert">
+			{props.text}
+		</div>
+	);
+};
+
+Alert.propTypes = {
+	color: PropTypes.string,
+	text: PropTypes.string,
+};
+
+// Here is where the <Alert /> component is being used, you don't have to edit this part
+// But it helps you understand what properties are being passed to the component
+ReactDOM.render(
+	<div>
+		<Alert text="OMG! Something really bad has happened!" color="red" />
+		<Alert text="Well, it is not that bad after all!" color="orange" />
+	</div>,
+	document.querySelector("#myDiv")
+);
