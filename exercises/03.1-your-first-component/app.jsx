ed16ae7f64c062d21818a2ef116cbd46a8f0a804
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-10-18 14:03:04 +0200

    Update app.jsx

--- exercises/03.1-your-first-component/app.jsx
+++ exercises/03.1-your-first-component/app.jsx
@@ -5,5 +5,5 @@ export const PrintHello = () => {
 	return <h1>I Love React</h1>;
 };
 
-// change the syntax of the first parameter to make it <PrintHello /> instead of PrintHello()
+// Change the syntax of the first parameter to make it <PrintHello /> instead of PrintHello()
 ReactDOM.render(PrintHello(), document.querySelector("#myDiv"));
