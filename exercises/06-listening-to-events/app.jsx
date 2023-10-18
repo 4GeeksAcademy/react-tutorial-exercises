e6d8ef469ba4655f49448b493cbea686749cbd54
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-10-18 20:36:39 +0200

    Update app.jsx

--- exercises/06-listening-to-events/app.jsx
+++ exercises/06-listening-to-events/app.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
 
 const clickHandler = e => {
 	console.log("I was clicked!", e);
 };
 
-const Alert = props => {
+const Alert = () => {
 	return <button className="btn btn-success btn-lg">Click Me</button>;
 };
 
-// here is where the alert component is being used, you don't have to edit this part,
-// but it helps you understand what properties is the component using
+// You don't have to edit anything below
 ReactDOM.render(<Alert />, document.querySelector("#myDiv"));
