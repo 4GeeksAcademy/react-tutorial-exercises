dae02f6ea2f47ebc4fac8547e88b4fd7c71776d7
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-10-18 19:52:50 +0200

    Update app.jsx

--- exercises/05-adding-styles/app.jsx
+++ exercises/05-adding-styles/app.jsx
@@ -10,7 +10,8 @@ const mySuperStyles = {
 const Badge = props => {
 	return (
 		<button style={mySuperStyles} type="button" className="btn btn-primary">
-			{props.label}<span className="badge badge-light">{props.number}</span>
+			{props.label}
+			<span className="badge badge-light">{props.number}</span>
 		</button>
 	);
 };
