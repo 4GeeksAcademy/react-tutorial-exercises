fe40085ab1d8c2ee836591b41714290e0178d88b
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-10-18 20:29:20 +0200

    Create solution.hide.jsx

---
+++ exercises/06-listening-to-events/solution.hide.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const clickHandler = (e) => {
+	console.log("I was clicked!", e);
+};
+
+const Alert = () => {
+	return (
+		<button className="btn btn-success btn-lg" onClick={clickHandler}>
+			Click Me
+		</button>
+	);
+};
+
+// You don't have to edit anything below
+ReactDOM.render(<Alert />, document.querySelector("#myDiv"));
