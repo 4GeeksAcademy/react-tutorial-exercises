7e7a30589550b5bc966ceb5bc793f1eb5c9b0c1d
Jose <126101249+JoseGeek78@users.noreply.github.com>
2023-10-25 17:56:08 +0000

    update

--- .learn/resets/01-hello-world/app.jsx
+++ .learn/resets/01-hello-world/app.jsx
@@ -3,11 +3,7 @@ import React from "react"; //Main React.js library
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 
 // WHAT: This variable returns contains the html to render
-let output = (
-	<span>
-		James is <strong>12</strong> years old
-	</span>
-);
+let output = <span>James is 12 years old</span>;
 
 // WHERE: A DOM element that will contain the entire react generated html
 const myDiv = document.querySelector("#myDiv");
