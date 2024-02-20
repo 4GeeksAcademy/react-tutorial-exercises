3d164067a1bf1c655349f698a06f4164b1220e66
Anggie Alava <131820456+AnggieAlava@users.noreply.github.com>
2024-02-20 04:25:47 +0000

    Corregido datos en app.jsx Jumbotron

--- exercises/03.4-jumbotron/app.jsx
+++ exercises/03.4-jumbotron/app.jsx
@@ -1,35 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 const HeroSection = (props) => {
-	// Here you have to return expected html using the properties being passed to the component
-	return (
-		<div className="bg-light p-5 m-5">
-			<h1 className="display-4">{props.title}</h1>
-			<p className="lead">{props.description}</p>
-			<a className="btn btn-primary btn-lg" href={props.buttonURL} role="button">
-				{props.buttonLabel}
-			</a>
-		</div>
-	);
+  // Here you have to return expected html using the properties being passed to the component
 };
 
 HeroSection.propTypes = {
-	// PropTypes here
-	title: PropTypes.string,
-	description: PropTypes.string,
-	buttonLabel: PropTypes.string,
-	buttonURL: PropTypes.string,
+  // PropTypes here
+  title: PropTypes.string,
 };
 
 ReactDOM.render(
-	<HeroSection
-		title="Welcome to React!"
-		description="React is the most popular rendering library in the world"
-		buttonLabel="Go to the official website"
-		buttonURL="https://reactjs.org/"
-	/>,
+  <HeroSection
+    title="Welcome to React!"
+    description="React is the most popular rendering library in the world"
+    buttonLabel="Go to the official website"
+    buttonURL="https://reactjs.org/"
+  />,
 
-	document.querySelector("#myDiv")
+  document.querySelector('#myDiv')
 );
