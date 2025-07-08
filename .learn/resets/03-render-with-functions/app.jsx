b01a991e371683646867edd2184cd66a774a4a83
Elkin Botero <147097939+eybagit@users.noreply.github.com>
2025-07-08 03:51:50 +0000

    EjerciciosRealizados

--- .learn/resets/03-render-with-functions/app.jsx
+++ .learn/resets/03-render-with-functions/app.jsx
@@ -4,8 +4,8 @@ import ReactDOM from "react-dom"; // We use ReactDOM to render into the DOM
 
 // This function returns a string that will be rendered
 export const PrintHello = () => {
-	return <h1>I Love React</h1>;
+	return <h1>Hello World</h1>;
 };
 
 //              what           where
-ReactDOM.render(<PrintHello />, document.querySelector("#myDiv"));
+ReactDOM.render(PrintHello(), document.querySelector("#myDiv"));
